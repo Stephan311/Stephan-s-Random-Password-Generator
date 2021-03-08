2115d1eee9f8e935058fd2a655f3808d74b5be4e
stephan du Plooy <sdpphotography1@gmail.com>
2021-03-07 19:25:15 -0500

    Wrote conditions for 3 and 2 options picked

--- script.js
+++ script.js
@@ -45,7 +45,53 @@ generateBtn.addEventListener("click", writePassword);
       };
 
       if (!choosenumber && !choosesymbols && !chooseuppercase && !chooselowercase) {
-        choice = alert("Atleast one of the criteria must be chosen")
+        choice = alert("Atleast one of the criteria has to be picked")
+      }
+
+      else if (choosenumber && choosesymbols && chooseuppercase && chooselowercase) {
+        choice = numbers.concat(symbols, uppercase, lowercase);
+      }
+
+      //conditions for 3 options picked
+
+      else if (choosenumber && choosesymbols && chooseuppercase) {
+        choice = numbers.concat(symbols, uppercase);
+      }
+      else if (choosenumber && choosesymbols && chooselowercase) {
+        choice = numbers.concat(symbols, lowercase);
+      }
+      else if (choosenumber && chooseuppercase && chooselowercase) {
+        choice = numbers.concat(uppercase, lowercase);
+      }
+      else if (choosesymbols && chooseuppercase && chooselowercase) {
+        choice = symbols.concat(uppercase, lowercase);
+      }
+
+      //conditions for 2 options picked
+
+      else if (choosenumber && choosesymbols) {
+        choice = numbers.concat(symbols);
+      }
+      else if (choosenumber && chooseuppercase) {
+        choice = numbers.concat(uppercase);
+      }
+      else if (choosenumber && chooselowercase) {
+        choice = numbers.concat(lowercase);
+      }
+      else if (choosesymbols && chooseuppercase) {
+        choice = symbols.concat(uppercase);
+      }
+      else if (choosesymbols && chooselowercase) {
+        choice = symbols.concat(lowercase);
+      }
+      else if (chooseuppercase & chooselowercase) {
+        choice = uppercase.concat(lowercase);
+      }
+
+      //conditions for 1 option picked
+
+      else if (choosenumber) {
+        choice = 
       }
 }
 
